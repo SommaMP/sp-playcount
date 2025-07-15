cd781f8780e8d183c5528cef355b3042175cefb8
SommaMP <alessiosomma111@gmail.com>
2025-07-15 10:53:23 +0200

    .

--- src/routes/album.js
+++
@@ -1,9 +0,0 @@
-// src/routes/album.js
-import express from 'express';
-import { getAlbum } from '../controllers/albumController.js';
-
-const router = express.Router();
-
-router.get('/', getAlbum);
-
-export default router;
